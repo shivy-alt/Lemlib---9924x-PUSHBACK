4074d60f496eec6baa295d2536ca519f5af23de1
not_shiv <151963917+shivy-alt@users.noreply.github.com>
2025-05-15 18:30:43 -0700

    added robodash, auto selector, and console

--- include/main.h
+++ include/main.h
@@ -78,3 +78,5 @@ void opcontrol(void);
 #endif
 
 #endif  // _PROS_MAIN_H_
+
+#include "robodash/api.h"
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
 
+rd::Selector selector({
+    {"Right side", right_side()},
+});
+rd::Console console;
+
 // controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
@@ -11,7 +16,7 @@ pros::MotorGroup leftMotors({-5, 4, -3},
 pros::MotorGroup rightMotors({6, -9, 7}, pros::MotorGearset::blue); // right motor group - ports 6, 7, 9 (reversed)
 
 // Inertial Sensor on port 10
-pros::Imu imu(10);
+pros::Imu inertial_mu(10);
 
 // tracking wheels
 // horizontal tracking wheel encoder. Rotation sensor, port 20, not reversed
@@ -132,32 +137,8 @@ ASSET(example_txt); // '.' replaced with "_" to make c++ happy
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-    // Move to x: 20 and y: 15, and face heading 90. Timeout set to 4000 ms
-    chassis.moveToPose(20, 15, 90, 4000);
-    // Move to x: 0 and y: 0 and face heading 270, going backwards. Timeout set to 4000ms
-    chassis.moveToPose(0, 0, 270, 4000, {.forwards = false});
-    // cancel the movement after it has traveled 10 inches
-    chassis.waitUntil(10);
-    chassis.cancelMotion();
-    // Turn to face the point x:45, y:-45. Timeout set to 1000
-    // dont turn faster than 60 (out of a maximum of 127)
-    chassis.turnToPoint(45, -45, 1000, {.maxSpeed = 60});
-    // Turn to face a direction of 90º. Timeout set to 1000
-    // will always be faster than 100 (out of a maximum of 127)
-    // also force it to turn clockwise, the long way around
-    chassis.turnToHeading(90, 1000, {.direction = AngularDirection::CW_CLOCKWISE, .minSpeed = 100});
-    // Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
-    // following the path with the back of the robot (forwards = false)
-    // see line 116 to see how to define a path
-    chassis.follow(example_txt, 15, 4000, false);
-    // wait until the chassis has traveled 10 inches. Otherwise the code directly after
-    // the movement will run immediately
-    // Unless its another movement, in which case it will wait
-    chassis.waitUntil(10);
-    pros::lcd::print(4, "Traveled 10 inches during pure pursuit!");
-    // wait until the movement is done
-    chassis.waitUntilDone();
-    pros::lcd::print(4, "pure pursuit finished!");
+    console.printf("The robot heading is %f\n", inertial_mu.get_heading());
+    selector.run_auton();
 }
 
 /**
